fe54bd1b2b8f0fca1f5622ee300e31d479eb1541
JannuIba17 <jannath.sarahibrat>
2025-11-06 19:07:28 +0530

    feat: added day 7 and day 8 files

--- JavaLearning/src/com/Day6/Collections/RemoveDuplicates.java
+++ JavaLearning/src/com/Day6/Collections/RemoveDuplicates.java
@@ -1,11 +1,13 @@
 package com.Day6.Collections;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
         String[] fruits={"Apple","Orange","Banana","Apple","Orange","Banana","Pineapple"};
+        System.out.println("Original Array: "+ Arrays.asList(fruits));
         Set<String> hs=new HashSet<>();
         for(String temp:fruits){
             hs.add(temp);
